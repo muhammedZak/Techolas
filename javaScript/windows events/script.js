07f4d62c5ecdf99bb87698f98cb0632923ed7750
Muhammed Zakariya <zakariya.zaku018@gmail.com>
2026-04-03 20:09:25 +0530

    windows events

---
+++ javaScript/windows events/script.js
@@ -0,0 +1,43 @@
+// // console.log(this);
+
+// // window.onload = function () {
+// //   alert('Page Loaded');
+// // };
+
+// // onresize = function () {
+// //   document.body.style.background = 'lightblue';
+// // };
+
+// // onscroll = function () {
+// //   if (scrollY > 200) {
+// //     document.getElementById('topButton').style.display = 'block';
+// //   } else {
+// //     document.getElementById('topButton').style.display = 'none';
+// //   }
+// // };
+
+// document.getElementById('vd').addEventListener('play', () => {
+//   alert('Playing.....');
+// });
+// document.getElementById('vd').addEventListener('pause', () => {
+//   alert('Poused');
+// });
+// document.getElementById('vd').addEventListener('ended', () => {
+//   alert('ended');
+// });
+
+// const arr = [1, 2, 3, 4, 5];
+// const val = arr.find((item) => item > 3);
+// console.log(val);
+
+// console.log(arr.some((item) => item > 2));
+// console.log(arr.some((item) => item > 10));
+
+// console.log(arr.every(item => item > 10))
+
+// const res = arr.join(', ')
+// console.log(res)
+
+// console.log(arr.slice(1, 3));
+// console.log(arr.includes(2))
+// arr.findIndex
